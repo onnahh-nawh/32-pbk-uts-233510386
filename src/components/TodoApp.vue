a29f2730dbfaaadff63f4bb7f1529d5ea61de6bf
onnahh-nawh <refionaasririzky01@gmail.com>
2025-05-08 22:15:13 +0700

    menginisialisasi variabel

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -6,6 +6,13 @@
 
     <script setup>
       import { ref, computed, onMounted } from 'vue';
+
+      const listTitle = ref('List Harian Gwh');
+      const showAddForm = ref(false);
+      const tasks = ref([]);
+      const newTask = ref({ ... });
+      const filterType = ref('all');
+
     </script>
   </div>
 </template>
